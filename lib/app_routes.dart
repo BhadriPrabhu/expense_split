c5e628a5e75db7dc91b3f79871b2bdcad0e80f6c
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-08-07 12:20:00 +0530

    fix: error fix and added fallback in app routes

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:split_arch/bottom_nav_screen.dart';
 import 'package:split_arch/features/addGroup/presentation/pages/add_group_screen.dart';
+import 'package:split_arch/features/groups/domain/entities/group_list.dart';
 // import 'package:flutter/widgets.dart';
 import 'package:split_arch/features/groups/presentation/pages/group_screen.dart';
 
@@ -9,20 +10,35 @@ class AppRoutes {
   static const String bottomNav = '/bottomNav';
   static const String addGroup = '/addGroup';
 
-  static Route<dynamic> generateRoute(RouteSettings settings){
-    final arg = settings.arguments;
-    switch(settings.name){
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case group:
-      return MaterialPageRoute(builder: (_) => GroupScreen(),);
+        return MaterialPageRoute(builder: (_) => const GroupScreen());
 
       case addGroup:
-      return MaterialPageRoute(builder: (_) => AddGroupScreen(),);
+        return MaterialPageRoute(builder: (_) => const AddGroupScreen());
 
       case bottomNav:
-      return MaterialPageRoute(builder: (_) => BottomNavScreen(groupId: (arg as String?) ?? ''), settings: settings);
+        final arg = settings.arguments;
+        if (arg is GroupList) {
+          return MaterialPageRoute(
+            builder: (_) => BottomNavScreen(group: arg),
+            settings: settings,
+          );
+        }
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Missing group data for this route')),
+          ),
+          settings: settings,
+        );
 
       default:
-      return MaterialPageRoute(builder: (_) => Scaffold(body: Center(child: Text("No route is defined for ${settings.name}"),),));
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route is defined for ${settings.name}')),
+          ),
+        );
     }
   }
 }
